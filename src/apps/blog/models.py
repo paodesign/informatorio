d5e78706dc5dd21184d3ee38abfd6871d729001a
paola <paomedina_88@hotmail.com>
2021-02-19 18:23:00 -0300

    probando modelo blog

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Autor(models.Model):
+    identificador = models.IntegerField(primary_key = True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_publicacion = models.DateTimeField()
+    categoria = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT)
+
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+	    return self.titulo
+
+
+
+
