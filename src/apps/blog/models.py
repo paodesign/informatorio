97ad858fb3ab47307553308ff3da7e25a0acbe4c
SantiJ <santijunco4@gmail.com>
2021-02-20 19:01:46 -0300

    'Creacion_modelos'

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -2,30 +2,24 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Autor(models.Model):
-    identificador = models.IntegerField(primary_key = True)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    def __str__(self):
-        return self.nombre
-
-
-
 class Post(models.Model):
+    id=models.AutoField(primary_key=True)
+    #id_user = models.ForeignKey('Usuario',on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
+    contenido = models.TextField()
     fecha_publicacion = models.DateTimeField()
     categoria = models.CharField(max_length=100)
-    autor = models.ForeignKey(Autor, on_delete=models.PROTECT)
-
-    
+        
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-	    return self.titulo
+        return self.titulo
 
-
-
-
+class Comentario(models.Model):
+    id = models.AutoField(primary_key=True)
+    #id_user = models.ForeignKey('Usuario',on_delete=CASCADE)
+    id_post = models.ForeignKey('Post',on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField()
