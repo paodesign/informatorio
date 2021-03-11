9daa51e865d8cfbef749f4561e33d144c84dc8fb
aquilesambrosig <aquiles-ambrosig@hotmail.com>
2021-03-11 13:04:26 -0300

    Ahora funciona con imagenes 2

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     fecha_publicacion = models.DateField(auto_now_add=True)
     #hora = models.TimeField(blank = True,null=True)
     categoria = models.ForeignKey('Categoria', to_field='categoria_nombre', on_delete = models.SET_NULL, null=True)
-    imagen = models.ImageField(upload_to = 'post', blank = True)
+    imagen = models.ImageField(upload_to = 'post', null=True, blank = True)
     
     def publish(self):
         self.published_date = timezone.now()
